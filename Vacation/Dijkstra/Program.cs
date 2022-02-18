f34af21a674cd2c32ee2bd126c2dfcd925d5cd07
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-02-18 12:57:43 +0900

    다익스트라 알고리즘 수정

--- Vacation/Dijkstra/Program.cs
+++ Vacation/Dijkstra/Program.cs
@@ -7,24 +7,11 @@ using System.Collections.Generic;
 //걍 끝이야 ㅠㅠ
 namespace Dijkstra
 {
-    class Node
-    {
-        public int distance;
-        public int priority = int.MaxValue;
-        public bool isVisited = false;
-
-        public Node(int _distance)
-        {
-            distance = _distance;
-        }
-
-    }
-
-
+    #region 사용자 입력
 
     class Dijkstra
     {
-        
+
         private int[,] dijkstra;
         private int size;
         List<int> path = new List<int>();
@@ -59,18 +46,18 @@ namespace Dijkstra
                 {
                     if (visited[i])
                         continue;
-                    if (distance[i] == int.MaxValue)
+                    if (distance[i] == int.MaxValue || distance[i] >= closeNode)
                         continue;
 
-                    if(distance[i] < closeNode)
-                    {
-                        closeNode = distance[i];
-                        now = i;
-                    }
+
+                    closeNode = distance[i];
+                    now = i;
                 }
 
                 if (now == -1)
                     break;
+
+                visited[now] = true;
 
                 for (int next = 0; next < size; next++)
                 {
@@ -80,7 +67,7 @@ namespace Dijkstra
                         continue;
 
                     int nextDist = distance[now] + dijkstra[now, next];
-                    if(nextDist < distance[next])
+                    if (nextDist < distance[next])
                     {
                         distance[next] = nextDist;
                         pastNodes[next] = now;
@@ -104,7 +91,7 @@ namespace Dijkstra
 
             foreach (int item in path)
             {
-                Console.WriteLine(path);
+                Console.WriteLine(item);
             }
         }
     }
@@ -115,26 +102,27 @@ namespace Dijkstra
         static void Main(string[] args)
         {
             Console.WriteLine("몇개의 노드를 만들 것이냐?");
+            Console.Write(">> ");
             if (int.TryParse(Console.ReadLine(), out int input))
             {
                 Dijkstra dijkstra = new Dijkstra(input);
                 for (int i = 0; i < input; i++)
                 {
-                    Console.WriteLine("");
+                    Console.WriteLine();
                     Console.WriteLine("행을 입력하세요");
-                    Console.Write(">>");
+                    Console.Write(">> ");
                     if (int.TryParse(Console.ReadLine(), out int row))
                     {
                         if (row > input)
                             return;
                         Console.WriteLine("열을 입력하세요");
-                        Console.Write(">>");
+                        Console.Write(">> ");
                         if (int.TryParse(Console.ReadLine(), out int col))
                         {
                             if (col > input)
                                 return;
                             Console.WriteLine("거리를 입력하세요");
-                            Console.Write(">>");
+                            Console.Write(">> ");
                             if (int.TryParse(Console.ReadLine(), out int distance))
                             {
 
@@ -144,84 +132,89 @@ namespace Dijkstra
                         }
                     }
                 }
+                dijkstra.Serch( input - 1, 0);
             }
         }
     }
+    #endregion
 }
 
 
 
-    //class DijkstraSerch
-    //{
-    //    const int nodeCount = 6;
-    //    int[,] nodes = new int[nodeCount, nodeCount]
-    //    {
-    //        {-1, 15, -1, 35, -1, -1},
-    //        {15, -1  5, 10, -1, -1},
-    //        {-1, 5, -1, -1, -1, -1},
-    //        {35, 10, -1, -1, 5, -1},
-    //        {-1, -1, -1, 5, -1, 5},
-    //        {-1, -1, -1, -1, 5, -1}
-    //    };
-    //    public void Serch(int startNum)
-    //    {
-    //        bool[] visited = new bool[nodeCount];
-    //        int[] distance = new int[nodeCount];
-    //        Array.Fill(distance, int.MaxValue);
-    //        int[] pastNode = new int[nodeCount];
+#region 입력 해놓기
+//class DijkstraSerch
+//{
+//    const int nodeCount = 6;
+//    int[,] nodes = new int[nodeCount, nodeCount]
+//    {
+//        {-1, 15, -1, 35, -1, -1},
+//        {15, -1  5, 10, -1, -1},
+//        {-1, 5, -1, -1, -1, -1},
+//        {35, 10, -1, -1, 5, -1},
+//        {-1, -1, -1, 5, -1, 5},
+//        {-1, -1, -1, -1, 5, -1}
+//    };
+//    public void Serch(int startNum)
+//    {
+//        bool[] visited = new bool[nodeCount];
+//        int[] distance = new int[nodeCount];
+//        Array.Fill(distance, int.MaxValue);
+//        int[] pastNode = new int[nodeCount];
 
-    //        distance[startNum] = 0;
-    //        pastNode[startNum] = startNum;
-    //        while (true)
-    //        {
-    //            int closer = int.MaxValue;
-    //            int now = -1;
+//        distance[startNum] = 0;
+//        pastNode[startNum] = startNum;
+//        while (true)
+//        {
+//            int closer = int.MaxValue;
+//            int now = -1;
 
-    //            for (int i = 0; i < nodeCount; i++)
-    //            {
-    //                if (visited[i])
-    //                    continue;
+//            for (int i = 0; i < nodeCount; i++)
+//            {
+//                if (visited[i])
+//                    continue;
 
-    //                if (distance[i] == int.MaxValue || distance[i] >= closer)
-    //                    continue;
+//                if (distance[i] == int.MaxValue || distance[i] >= closer)
+//                    continue;
 
-    //                closer = distance[i];
-    //                now = i;
-    //            }
-    //            if (now == -1)
-    //                break;
-    //             visited[now] = true;
-    //            Console.WriteLine($"{now}번째 정점에 방문했습니다");
+//                closer = distance[i];
+//                now = i;
+//            }
+//            if (now == -1)
+//                break;
+//             visited[now] = true;
+//            Console.WriteLine($"{now}번째 정점에 방문했습니다");
 
-    //            for (int next = 0; next < nodeCount; next++)
-    //            {
-    //                if (nodes[now, next] == 0)
-    //                    continue;
-    //                if (visited[next])
-    //                    continue;
+//            for (int next = 0; next < nodeCount; next++)
+//            {
+//                if (nodes[now, next] == 0)
+//                    continue;
+//                if (visited[next])
+//                    continue;
 
-    //                int nextDist = distance[now] + nodes[now, next];
+//                int nextDist = distance[now] + nodes[now, next];
 
-    //                if (nextDist < distance[next])
-    //                {
-    //                    distance[next] = nextDist;
-    //                    pastNode[next] = now;
-    //                }
-    //            }
+//                if (nextDist < distance[next])
+//                {
+//                    distance[next] = nextDist;
+//                    pastNode[next] = now;
+//                }
+//            }
 
-    //        }
-    //    }
+//        }
+//    }
 
-    //}
+//}
 
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        DijkstraSerch dijkstra = new DijkstraSerch();
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        DijkstraSerch dijkstra = new DijkstraSerch();
 
-    //        dijkstra.Serch(2);
+//        dijkstra.Serch(2);
 
-    //    }
-    //}
+//    }
+//}
+
+#endregion
 
